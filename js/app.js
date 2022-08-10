c8ff5a36f02929f9b857375ff0591bafa189df01
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-10 18:24:06 +0100

    ft-aurora-front-end-recruitment-task

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let navBarToggle = document.querySelectorAll('.nav-menu-list');
+let menu = document.querySelector('.menu');
+let navBarMenu = document.querySelector('.navbar-menu');
+
+navBarToggle.forEach(function(toggle) {
+  toggle.addEventListener('click', function() {
+    navBarToggle.forEach(function(toggle) {
+        toggle.classList.remove('active');
+        }
+    );
+    this.classList.add('active');
+  });
+});
+
+// show menu on click
+menu.addEventListener('click', function() {
+    navBarMenu.classList.toggle('show');
+    if (navBarMenu.classList.contains('show')) {
+        menu.classList.remove('fa-bars');
+        menu.classList.add('fa-xmark');
+    }
+    else {
+        menu.classList.remove('fa-xmark');
+        menu.classList.add('fa-bars');
+    }
+})
